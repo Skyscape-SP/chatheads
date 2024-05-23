58476c0657e63f6bc426299aa6947e533bfdbf31
Benjamin <76600137+IllagerCaptain@users.noreply.github.com>
2024-05-23 12:56:36 -0500

    me when 8-bit images

--- src/main/java/com/campersamu/chatheads/mixin/DownloadHeadOnJoin.java
+++ src/main/java/com/campersamu/chatheads/mixin/DownloadHeadOnJoin.java
@@ -81,7 +81,7 @@ public abstract class DownloadHeadOnJoin {
         //generate overlay
         for (int x = 40; x < 48; x++) {
             for (int y = 8; y < 16; y++) {
-                if (image.getRGB(x, y).getAlpha() > 25) {//vanilla minecraft disregards alpha of less than 10% so let's do that too
+                if (image.getRGB(x, y) >> 24 == 0xff) {
                     int rgb = image.getRGB(x, y);
                     playerHead[y - 8][x - 8] = fromRgb(rgb & 0xffffff);
                 }
